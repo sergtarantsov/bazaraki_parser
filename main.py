44541e00a7c2b376be22d3c4c509afd5a5fc87f6
Сергей Таранцов <starantsov@Sergejs-MacBook-Pro.local>
2023-08-16 19:09:14 +0300

    delete libs

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-from get_ad_count import get_ad_count
-from get_parsed_page import get_parsed_page
-from make_links_list import make_links_list
-from make_full_link_array import make_full_link_array
-from create_imprint import create_imprint
 from changes_check import changes_check
 
 main_link = 'https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/body-type---7/year_max---66/year_min---61/?price_min=12500&price_max=20000'
