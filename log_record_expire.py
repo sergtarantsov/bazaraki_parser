c0310e715e73c78675c89516200e772aab9551d6
Сергей Таранцов <starantsov@Sergejs-MacBook-Pro.local>
2023-08-17 15:21:12 +0300

    add ad details to expire page

--- log_record_expire.py
+++ log_record_expire.py
@@ -1,6 +1,8 @@
 import time
+from parse_ad_details import parse_ad_details
 
 def log_record_expire(link, log_file):
+    ad_details_array = parse_ad_details(link)
     with open(log_file, 'a') as file:
-        file.write(f"{'Expire'}; {time.ctime()}; {link}\n")
+        file.write(f"{'Expire'}; {time.ctime()}; {link}; {'; '.join(ad_details_array)}\n")
     file.close()
