0f0104fc3253ee81df653715581a1b505808519c
Сергей Таранцов <starantsov@Sergejs-MacBook-Pro.local>
2023-08-17 15:07:31 +0300

    add env filepath

--- changes_check.py
+++ changes_check.py
@@ -3,8 +3,12 @@ from log_record_add import log_record_add
 from log_record_expire import log_record_expire
 from create_imprint import create_imprint
 import time
+import os
 
 def changes_check(url, imprint_file):
+    base_directory = os.environ.get("PARSER_DIR")
+    log_path = os.path.join(base_directory, 'data/log_file.csv')
+    imprint_path = os.path.join(base_directory, 'data/imprint.csv')
     link_array = make_full_link_array(url)
     with open(imprint_file, 'r') as file:
         imprint_text=file.read()
@@ -14,10 +18,10 @@ def changes_check(url, imprint_file):
     expired_ads_array = [x for x in imprint_array if x not in link_array]
     if len(added_ads_array)>0:
         for link_element in added_ads_array:
-            log_record_add(link_element, 'data/log_file.csv')
+            log_record_add(link_element, log_path)
             time.sleep(5)  
     if len(expired_ads_array)>0:
         for link_element in expired_ads_array:
-            log_record_expire(link_element, 'data/log_file.csv')
+            log_record_expire(link_element, log_path)
             time.sleep(10)  
-    create_imprint('imprint.csv', link_array)
+    create_imprint(imprint_path, link_array)
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from changes_check import changes_check
+import os
+
+base_directory = os.environ.get('PARSER_DIR')
+print(base_directory)
+imprint_path = os.path.join(base_directory, 'data', 'imprint.csv')
+print(imprint_path)
 
 main_link = 'https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/body-type---7/year_max---66/year_min---61/?price_min=12500&price_max=20000'
 
-changes_check(main_link,'data/imprint.csv')
+changes_check(main_link,imprint_path)
