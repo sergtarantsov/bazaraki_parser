c7a3c232b1b875873e5a82a20380af842685a14f
Сергей Таранцов <starantsov@Sergejs-MacBook-Pro.local>
2023-08-22 19:02:16 +0300

    change main link

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ print(base_directory)
 imprint_path = os.path.join(base_directory, 'data', 'imprint.csv')
 print(imprint_path)
 
-main_link = 'https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/body-type---7/year_max---66/year_min---61/?price_min=12500&price_max=20000'
+main_link = 'https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/body-type---7/year_max---69/year_min---60/?price_min=12500&price_max=20000'
 
 changes_check(main_link,imprint_path)
