0151497ac70a04449a8a71e4ddea7c4b63b9966a
Сергей Таранцов <starantsov@Sergejs-MacBook-Pro.local>
2023-09-20 21:49:21 +0300

    add env variables

--- main.py
+++ main.py
@@ -13,3 +13,12 @@ log_path = os.path.join(base_directory, 'data/log_file.csv')
 imprint_path = os.path.join(base_directory, 'data/imprint.csv')
 
 changes_check(main_link,log_path, imprint_path)
+
+#ссылка на готовый фильтр в базараки
+main_link_2 = 'https://www.bazaraki.com/car-motorbikes-boats-and-parts/cars-trucks-and-vans/body-type---7/year_max---69/year_min---60/?price_min=12500&price_max=20000'
+#путь к файлу логов - в нем собираются новые и истекшие объявления
+log_path_2 = os.path.join(base_directory, 'data/log_file_2.csv')
+#путь к файлу отпечатка - для сравнения нового списка объявлений с предыдущим запросом
+imprint_path_2 = os.path.join(base_directory, 'data/imprint_2.csv')
+
+changes_check(main_link_2,log_path_2, imprint_path_2)
